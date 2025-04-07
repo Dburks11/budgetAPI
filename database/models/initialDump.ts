61f4b65637c3f0f8b1af252d26da1c21f730ae05
dburks11 <drb2006@outlook.com>
2025-04-06 21:13:13 -0500

    changes

--- database/models/initialDump.ts
+++ database/models/initialDump.ts
@@ -1,2 +1,38 @@
 import { DataTypes, Model } from "sequelize";
 import sequelize from "../sequelize";
+
+class Transaction extends Model {
+  public id!: string;
+  public date!: string;
+  public amount!: number;
+  public payee!: string;
+}
+
+Transaction.init(
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    payee: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: "transactions",
+    timestamps: false, // Disable createdAt/updatedAt fields
+  }
+);
+
+export default Transaction;
